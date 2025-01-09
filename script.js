f72cc5ced754fe263af74d90b168f25ae3e88f49
Stephen Anaba <98113361+3takita@users.noreply.github.com>
2025-01-09 16:18:06 +0000

    Building Converter function

--- script.js
+++ script.js
@@ -88,5 +88,109 @@ function priority(ch) {
     return val;
 }
 
-
+//7. postConcition: Accepts op as arg & prompts the stack to pop according to rules conditions
+function converter(infix) {
+    //if infix is empty, abort
+    if(infix.length===0) {
+        console.log("Infix is empty"); 
+        return;
+    } 
+    //variables
+    let a=0,sho=0,n=0; 
+    let done=false; 
+    let staq=""; 
+    //option of `result only` or `how result was reached` & `the result`
+//    console.log("Choose display:\n\t(1) Only display result\n\t(2) Display working & result\nOption: ");
+//    cin>>sho;
+//    cout<<endl;
+    //loop
+    for(int i=0; i<infix.length; i++) {
+        let ch = infix[i], next = infix[i+1], prev=infix[i-1]; 
+        //if ch is alfa 
+        if(isalpha(ch)) {   
+            postfix += ch; //put it in postfix
+            sho==2 ? console.log(ch+" is an alphabet. Put it in postix. {" + staq + " \t " + postfix + "}\n") : console.log(""); 
+        }//if ch is a number
+        else if(isdigit(ch) || ch=='.') {   
+            postfix += ch; //put it in postfix
+            sho==2 ? consoloe.log(ch<<" is a number. Put it in postfix. {"<<staq<<" \t "<<postfix<<"}\n"<<endl : cout<<""; 
+        }
+        //if ch is (:
+        else if(ch == '(') { 
+            stak.push(ch); //put it in stack
+            staq += ch;   
+            a++; 
+            sho==2 ? cout<<ch<<" is open-parenthesis. Put it in the stack. {"<<staq<<" \t "<<postfix<<"}\n"<<endl : cout<<""; 
+        }
+        //if ch is ) && a>1:
+        else if(ch == ')' /*&& a>1*/) { 
+            sho==2 ? cout<<ch<<" is close-parenthesis. Pop enclosed operator(s) & "<<ch : cout<<""; 
+            //pop everything before ( into postfix except '('
+            while(stak.top() != '(') {
+                stak.top()!='(' && stak.top()!=')' ? (postfix += stak.top()) : (postfix=postfix);
+                //postfix += stak.top();
+                stak.pop();
+                staq.erase(staq.length()-1,1); 
+            }
+            stak.pop(); //then pop off ( into the air or som'n
+            stak.empty()? staq="" : staq.erase(staq.length()-1,1); 
+            a=0;
+            sho==2 ? cout<<". {"<<staq<<" \t "<<postfix<<"}\n"<<endl : cout<<""; 
+        }
+        //if ch is operator:
+        else if(isOp(ch)) {
+            //else if stak is not empty
+                int k=0; string pre="";
+            while(true) {
+                k>0?pre="\t":pre=""; //for proper spacing after 1st iteration
+                sho==2 ? cout<<pre<<ch<<" is operator. " : cout<<""; 
+                //if stack is empty
+                if(stak.empty()) {  
+                    sho==2 ? cout<<"Stack is empty, so put "<<ch<<" in stack. ": cout<<"";
+                    stak.push(ch);
+                    staq += ch;       
+                    a>0 ? a++ : a=a; //increment a when op is added to stak
+                    sho==2 ? cout<<"{"<<staq<<"\t"<<postfix<<"}\n"<<endl : cout<<""; 
+                    break;
+                }
+                else { //if priority(stak.top) >= priority(ch):
+                    if(priority(stak.top()) >= priority(ch)) {
+                        //move stak.top to postfix
+                        sho==2 ? cout<<"Priority of "<<stak.top()<<" >= priority of "<<ch<<"), so move "<<stak.top()<<" into postfix. " : cout<<"";
+                        //move stack.top to postfix
+                        stak.top()!='(' && stak.top()!=')' ? (postfix += stak.top()) : (postfix=postfix );
+                        //postfix += stak.top();
+                        stak.pop();
+                        stak.empty()? staq="" : staq.erase(staq.length()-1,1); 
+                        sho==2 ? cout<<"{"<<staq<<"\t"<<postfix<<"}\n"<<endl : cout<<""; 
+                    }	
+                    //if (priority of stack.top) < (priority of ch):
+                    else if(priority(stak.top()) < priority(ch)) {
+                        sho==2 ? cout<<"Priority of "<<stak.top()<<" < priority of "<<ch<<", so put "<<ch<<" in stack. " : cout<<""; 		    
+                        stak.push(ch); //put ch in stack
+                        staq += ch;   
+                        a>0 ? a++ : a=a; //increment a when op is added to stak
+                        sho==2 ? cout<<"{"<<staq<<"\t"<<postfix<<"}\n"<<endl : cout<<""; 
+                        break;
+                    }//close last if statement
+                    else {cout<<"Not Captured"<<endl; break;}
+                }
+                k++;
+            } 
+        } 
+    } 
+    
+    //if @ this point stack isNot empty, move everything to postfix
+    if(stak.size()>0) { 
+        while(stak.size()>0) {
+            //pop all in stack into postfix
+            stak.top()!='(' && stak.top()!=')' ? (postfix += stak.top()) : (postfix=postfix );
+            //postfix += stak.top();
+            sho==2 ? cout<<"\tput "<<stak.top()<<" in postfix." : cout<<""; 
+            stak.pop();
+            staq.erase(staq.length()-1,1); 
+            sho==2 ? cout<<" {"<<staq<<"\t"<<postfix<<"}"<<endl : cout<<""; 
+        }
+    }
+}
 
